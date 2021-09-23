ad8da95809e3ccbe87e1fb7550af9327dbc65371
Inpyo Hong <y2kpaulh@epiens.com>
2021-09-23 14:36:51 +0900

    Initial Commit

---
+++ Shared/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  Shared
+//
+//  Created by Inpyo Hong on 2021/09/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
